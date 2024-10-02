f2d014247d71de75fb7aa518b3e6b57f61e9fd6d
Quang Nguyen <nguyenhaiquang3@gmail.com>
2024-10-02 18:20:43 -0400

    Get data from server

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { ChangeEvent, SyntheticEvent, useState } from "react";
 import "./App.css";
 import CardList from "./Components/CardList/CardList";
 import Search from "./Components/Search/Search";
+import { CompanySearch } from "./company";
+import { searchCompanies } from "./api";
+
 function App() {
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
+  const [serverError, setServerError] = useState<string>("");
 
   // get data from input and display in the console
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -11,9 +16,18 @@ function App() {
     console.log(e);
   };
 
-  const onClick = (e: SyntheticEvent) => {
-    console.log(e);
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    
+    // check see if server return string or array of data
+    if(typeof result === "string") {
+      setServerError(result);
+    } else if(Array.isArray(result.data)){
+      setSearchResult(result.data);
+    }
+    console.log(searchResult)
   };
+
   return (
     <div className="App">
       <Search onClick={onClick} search={search} handleChange={handleChange} />
--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -20,7 +20,7 @@ const Search: React.FC<Props> = ({
   return (
     <div>
       <input value={search} onChange={(e) => handleChange(e)}></input>
-      <button onClick={(e) => onClick(e)}>Click</button>
+      <button onClick={(e) => onClick(e)} />
     </div>
   );
 };
